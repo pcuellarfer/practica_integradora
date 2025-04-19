50edfc2246b400d23acc390db76a539024d93c4b
pcuellar <pcuellar@educa.madrid.org>
2025-04-19 22:41:27 +0200

    controller para prueba de añadir una opcion de ver detalle en dashboard

---
+++ administracion-app/src/main/java/org/pcuellar/administracionapp/controller/DashboardController.java
@@ -0,0 +1,25 @@
+package org.pcuellar.administracionapp.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.pcuellar.administracionapp.dto.Usuario.UsuarioDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @GetMapping("/detalle")
+    public String verMisDetalles(HttpSession session, Model model) {
+        UsuarioDTO susuario = (UsuarioDTO) session.getAttribute("susuario");
+
+        if (susuario == null) {
+            return "redirect:/login";
+        }
+
+        model.addAttribute("usuario", susuario);
+        return "";
+    }
+}
